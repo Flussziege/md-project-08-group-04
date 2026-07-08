16fbf9599d3f62a481cee861b96a97cefc90aa35
Flussziege <flusszieger@gmail.com>
2026-07-08 13:31:13 +0200

    fine tuning der function

--- minimise_algo.py
+++ minimise_algo.py
@@ -17,7 +17,7 @@ def backtracking_line_search(
     alpha = alpha0
 
     for _ in range(50):
-        x_new = x + alpha * p
+        x_new = np.mod(x + alpha * p, sim.box_length)
         E_new = energy_func(x_new, ps, sim)
 
         if E_new < E_old:
@@ -46,7 +46,8 @@ def minimise_starting_position(
         sim: LJ_gas.SimulationParameters, 
         SD: bool = False,
         tolerance =1e-5,
-        alpha_SD = 0.5
+        alpha_SD = 1e-5,
+        max_steps = 1000
         ):
 
     #Listen um Werte zu speichern
@@ -69,10 +70,13 @@ def minimise_starting_position(
     p_hist.append(p.copy())   
     pos_hist.append(ps.position.copy())
     E_hist.append(E)
-    Fmax_hist(max_force_norm(ps.force))
+    Fmax_hist.append(max_force_norm(ps.force))
+
+    #just to be safe
+    step = 0
 
 
-    while max_force_norm(ps.force) >= tolerance: 
+    while max_force_norm(ps.force) >= tolerance and step < max_steps: 
         # alpha bestimmen
         # position ändern
         # neue Kraft berechnen
@@ -82,36 +86,76 @@ def minimise_starting_position(
         if SD:
             alpha = alpha_SD
         else: 
-            alpha = backtracking_line_search(
+           alpha = backtracking_line_search(
             x=ps.position,
             p=p,
+            ps=ps,
+            sim=sim,
             E_old=E,
             energy_func=energy_func,
             alpha0=1e-4
-        )
+            )
+           
+        if alpha == 0.0:
+            print("Keine passende Schrittweite gefunden.")
+            break
         
 
-    #Schritt gehen
-    ps.position = ps.position + alpha * p 
+        #Schritt gehen
+        ps.position = ps.position + alpha * p 
 
-    #neue Kräfte berechnen
-    LJ_gas.calculate_force(ps, sim)
-    E = LJ_gas.potential_energy(ps, sim)
+        #periodic boundary anwenden
+        LJ_gas.apply_periodic_boundary(ps, sim)
 
-    if SD:
-        p = ps.force.copy()
-    else:
-        beta = (ps.force.transpose @ (ps.force - p))/(p.transpose @ p) #Form: Skalar
-        beta = max(beta, 0)
-        p = ps.force + beta * p
+        #für berechnung von beta speichern
+        F_old = ps.force.copy()
+        p_old = p.copy()
 
-    #neuen Werte speichern
-    p_hist.append(p.copy())   
-    pos_hist.append(ps.position.copy())
-    E_hist.append(E)
-    Fmax_hist(max_force_norm(ps.force))
-    alpha_hist.append(alpha)
-    beta_hist.append(beta)
+        #neue Kräfte berechnen
+        LJ_gas.calculate_force(ps, sim)
+        E = LJ_gas.potential_energy(ps, sim)
+
+        if SD:
+            p = ps.force.copy()
+            beta = 0.0
+        else:
+            #beta berechnen mit toller formel
+            beta = np.sum(ps.force * (ps.force - F_old)) / np.sum(F_old * F_old)
+
+            #darf halt nciht negativ sein
+            beta = max(beta, 0.0)
+
+            p_new = ps.force + beta * p_old
+
+
+            #wenn CG keinen fortschritt gemacht werden kann,
+            #wird auf SD zurückgegriffen und beta = 0
+            if np.sum(ps.force * p_new) <= 0: 
+                p_new = ps.force.copy()
+                beta = 0.0
+            
+            #Wert übernehmen
+            p = p_new.copy()
+
+        #neuen Werte speichern
+        p_hist.append(p.copy())   
+        pos_hist.append(ps.position.copy())
+        E_hist.append(E)
+        Fmax_hist.append(max_force_norm(ps.force))
+        alpha_hist.append(alpha)
+        beta_hist.append(beta)
+
+        step += 1
+
+    return {
+        "positions": ps.position.copy(),
+        "E_hist": np.array(E_hist),
+        "Fmax_hist": np.array(Fmax_hist),
+        "pos_hist": np.array(pos_hist),
+        "alpha_hist": np.array(alpha_hist),
+        "p_hist": np.array(p_hist),
+        "beta_hist": np.array(beta_hist),
+    }
 
 
         
